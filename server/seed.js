b73c71840a66787607306cc5a70332837f764694
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-01-08 17:03:00 -0600

    started database

---
+++ server/seed.js
@@ -0,0 +1,70 @@
+require('dotenv').config() 
+const {CONNECTION_STRING} = process.env 
+const Sequelize = require('sequelize') 
+const sequelize = new Sequelize(CONNECTION_STRING) 
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+        drop table if exists users;
+        drop table if exists patient;
+        drop table if exists xray_order;
+        drop table if exists blood_order;
+        drop table if exists ekg_order;
+
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(20),
+            password VARCHAR(500)
+        );
+
+        CREATE TABLE patient (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL REFERENCES users(id),
+            first_name VARCHAR(20),
+            last_name VARCHAR(20),
+            date_of_birth DATE,
+            gender BOOLEAN
+        );
+
+        CREATE TABLE xray_order (
+            id SERIAL PRIMARY KEY,
+            patient_id INTEGER NOT NULL REFERENCES patient(id),
+            completed BOOLEAN
+        );
+
+        CREATE TABLE blood_order (
+            id SERIAL PRIMARY KEY,
+            patient_id INTEGER NOT NULL REFERENCES patient(id),
+            completed BOOLEAN
+        );
+
+        CREATE TABLE EKG_order (
+            id SERIAL PRIMARY KEY,
+            patient_id INTEGER NOT NULL REFERENCES patient(id),
+            completed BOOLEAN
+        );
+
+        CREATE TABLE ortho_order (
+            id SERIAL PRIMARY KEY,
+            patient_id INTEGER NOT NULL REFERENCES patient(id),
+            completed BOOLEAN
+        );
+
+        insert into users (username, password)
+        values ('Jenny', 'Jenny123'),
+            ('Dewey', 'Dewey123'),
+            ('Pamela', 'Pamela123'),
+            ('Billy', 'Billy123');
+
+        insert into patient (user_id, first_name, last_name, date_of_birth, gender)
+        values (1, 'Jimmy', 'Smith', 10/03/1980, true),
+            (2, 'Sally', 'Hamilton', 05/06/1945, false),
+            (3, 'Jessica', 'Diver', 08/12/2012, false),
+            (2, 'Gary', 'Sprinkler', 03/22/1995, true);
+        `).then(() => {
+            console.log('DB seeded successfully!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const {SERVER_PORT} = process.env
+const {seed} = require('./seed.js')
 const {newPatient, getPatients, completeOrder} = require('./controller')
 
 const app = express()
@@ -7,9 +10,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post('/seed', seed)
+
 app.get('/api/getPatients', getPatients)
 app.post('/api/newPatient', newPatient)
 app.put('/api/completeOrder/:id', completeOrder)
 
 
-app.listen(4004, () => console.log('Up on 4004'))
+app.listen(SERVER_PORT, () => console.log(`Up on ${SERVER_PORT}`))
